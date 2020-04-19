ee1604cadd44845a965d945272755d8fc5e649e0
Zi-web <37513694+Zi-web@users.noreply.github.com>
2020-04-19 21:05:48 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,1 @@
-$(".btn--main").on("click",function(t){$("html,body").stop().animate({scrollTop:$(this.hash).offset().top},1e3),t.preventDefault()});
+$(".btn--scroll").on("click",function(t){$("html,body").stop().animate({scrollTop:$(this.hash).offset().top},1e3),t.preventDefault()});
